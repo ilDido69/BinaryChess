54483eaf406151ced244f0ef656dc288064d794c
ilDido69 <matyas.groppelli@gmail.com>
2026-06-08 21:11:50 +0200

    modified README

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-Requirements
+This is a simple chess GUI.
+
+Future features:
+- Improve the GUI
+- Add an engine
+- Improve the move generation
+
+Requirements:
 - C++17 or higher
 - CMake 3.16+
 - SFML 3.0
 - A C++ compiler (MSVC, GCC, Clang)
 
-Easy installation
+Easy installation:
 - Clone the repository
 - Install with vcpkg sfml 3.0
 - Compile with cmake
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
-
-/*
 #include "Game.h"
 #include "utils.h"
 #include "moveGenerator.h"
 #include <chrono>
 #include <iostream>
+#include <string>
 
-uint64_t perft(BoardState state, int depth) // passa per valore → copia automatica
+//0 = Game, 1 = perft
+int input()
+{
+    while (true)
+    {
+        std::string in;
+        std::cout << "0: Game,\n1: perft\nInput: ";
+        std::cin >> in;
+        if (in == "0")
+            return 0;
+        if (in == "1")
+            return 1;
+        std::cout << "Input error" << std::endl;
+    }
+}
+
+uint64_t perft(BoardState state, int depth)
 {
     if (depth == 0) return 1;
 
@@ -15,7 +30,7 @@ uint64_t perft(BoardState state, int depth) // passa per valore → copia automa
 
     for (const Move& move : moves)
     {
-        BoardState next = state; // copia la posizione
+        BoardState next = state;
         moveGenerator::applyMove(next, move);
         nodes += perft(next, depth - 1);
     }
@@ -24,9 +39,8 @@ uint64_t perft(BoardState state, int depth) // passa per valore → copia automa
 
 int main()
 {
-    bool testMode = false; // ← metti false per giocare
-
-    if (testMode)
+    int testMode = input();
+    if (testMode == 1)
     {
         BoardState state;
         state.board = {
@@ -60,24 +74,6 @@ int main()
     }
 
     Game game;
-    while (game.running())
-    {
-        game.update();
-        game.render();
-    }
-
-    return 0;
-}
-*/
-
-#include "Game.h"
-
-int main() {
-
-    //Initialize game object
-    Game game;
-
-    //Game loop
     while (game.running())
     {
         game.update();
